5aedb58ddad7942dde3fedde5b09cd82398b9e9e
ksw4015 <zealot4321@naver.com>
2024-11-22 16:25:48 +0900

    feat: add MainNavHost to MainActivity

--- app/src/main/java/kr/ksw/mybudget/presentation/main/MainActivity.kt
+++ app/src/main/java/kr/ksw/mybudget/presentation/main/MainActivity.kt
@@ -5,20 +5,30 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import kr.ksw.mybudget.presentation.main.navigation.MainBottomNavigation
+import kr.ksw.mybudget.presentation.main.navigation.MainNavHost
 import kr.ksw.mybudget.ui.theme.MyBudgetTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             MyBudgetTheme {
+                val navController = rememberNavController()
                 Scaffold(
-                    modifier = Modifier.fillMaxSize()
+                    modifier = Modifier.fillMaxSize(),
+                    bottomBar = {
+                        MainBottomNavigation(navController)
+                    }
                 ) { innerPadding ->
-
+                    MainNavHost(
+                        modifier = Modifier.padding(innerPadding),
+                        navController = navController
+                    )
                 }
             }
         }
