1250aa6ddb4e0446732120d8967d72106fe3ae1a
ksw4015 <zealot4321@naver.com>
2024-11-25 20:50:32 +0900

    feat: add AddActivity (empty activity)

---
+++ app/src/main/java/kr/ksw/mybudget/presentation/add/AddActivity.kt
@@ -0,0 +1,20 @@
+package kr.ksw.mybudget.presentation.add
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Surface
+import kr.ksw.mybudget.ui.theme.MyBudgetTheme
+
+class AddActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyBudgetTheme {
+                Surface {
+
+                }
+            }
+        }
+    }
+}
