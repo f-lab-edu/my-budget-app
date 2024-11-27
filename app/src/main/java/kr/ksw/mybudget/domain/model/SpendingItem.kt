53805892a02b82b34185c96a13cf40cd6881aff4
ksw4015 <zealot4321@naver.com>
2024-11-27 14:57:13 +0900

    feat: add string res id to enum class param

--- app/src/main/java/kr/ksw/mybudget/domain/model/SpendingItem.kt
+++ app/src/main/java/kr/ksw/mybudget/domain/model/SpendingItem.kt
@@ -3,10 +3,10 @@ package kr.ksw.mybudget.domain.model
 import java.time.LocalDate
 
 data class SpendingItem(
-    val id: Int?,
-    val title: String,
-    val date: LocalDate,
-    val price: Int,
-    val category: SpendingType,
+    val id: Int? = null,
+    val title: String = "",
+    val date: LocalDate = LocalDate.now(),
+    val price: Int = 0,
+    val category: SpendingType = SpendingType.UNKNOWN,
     val cardNum: String? = null,
 )
