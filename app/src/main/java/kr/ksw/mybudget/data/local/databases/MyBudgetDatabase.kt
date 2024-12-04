9315798224e327b54f0e627dea44da8fb6e4a5d1
ksw4015 <zealot4321@naver.com>
2024-12-04 14:41:25 +0900

    feat: add CardDao to Database

--- app/src/main/java/kr/ksw/mybudget/data/local/databases/MyBudgetDatabase.kt
+++ app/src/main/java/kr/ksw/mybudget/data/local/databases/MyBudgetDatabase.kt
@@ -4,14 +4,17 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import kr.ksw.mybudget.data.local.converter.Converters
+import kr.ksw.mybudget.data.local.dao.CardDao
 import kr.ksw.mybudget.data.local.dao.SpendingDao
+import kr.ksw.mybudget.data.local.entity.CardEntity
 import kr.ksw.mybudget.data.local.entity.SpendingEntity
 
 @Database(
-    entities = [SpendingEntity::class],
+    entities = [SpendingEntity::class, CardEntity::class],
     version = 1
 )
 @TypeConverters(Converters::class)
 abstract class MyBudgetDatabase: RoomDatabase() {
     abstract val spendingDao: SpendingDao
+    abstract val cardDao: CardDao
 }
