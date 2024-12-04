cd38827ea55a4eeb8258375e93f91eaa09acbcbd
ksw4015 <zealot4321@naver.com>
2024-12-04 18:11:39 +0900

    feat: add CardModule

--- app/src/main/java/kr/ksw/mybudget/data/di/CardModule.kt
+++ app/src/main/java/kr/ksw/mybudget/data/di/CardModule.kt
@@ -4,10 +4,17 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kr.ksw.mybudget.data.local.dao.CardDao
+import kr.ksw.mybudget.data.repository.card.CardRepository
+import kr.ksw.mybudget.data.repository.card.CardRepositoryImpl
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object CardModule {
-
+    @Provides
+    @Singleton
+    fun provideCardRepository(cardDao: CardDao): CardRepository {
+        return CardRepositoryImpl(cardDao)
+    }
 }
