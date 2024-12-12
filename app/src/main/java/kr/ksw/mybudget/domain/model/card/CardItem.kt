94f7ac0764149563e09a971cf6da3f6f44327190
ksw4015 <zealot4321@naver.com>
2024-12-12 14:10:35 +0900

    feat: add defalut value to CardItem

--- app/src/main/java/kr/ksw/mybudget/domain/model/card/CardItem.kt
+++ app/src/main/java/kr/ksw/mybudget/domain/model/card/CardItem.kt
@@ -2,12 +2,13 @@ package kr.ksw.mybudget.domain.model.card
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
+import kr.ksw.mybudget.data.local.entity.CARD_TYPE_CREDIT
 
 @Parcelize
 data class CardItem(
-    val id: Int,
-    val cardName: String,
-    val cardNumber: String,
-    val cardColor: Long,
-    val cardType: Int,
+    val id: Int = -1,
+    val cardName: String = "",
+    val cardNumber: String = "",
+    val cardColor: Long = 0xFFD29CB4,
+    val cardType: Int = CARD_TYPE_CREDIT,
 ): Parcelable
